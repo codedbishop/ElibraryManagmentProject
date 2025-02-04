9f6f960582e4752f581ba11a72ea5854ffbb683a
codedbishop <dustin.z.bishop@gmail.com>
2025-02-03 19:52:07 -0700

    Commit 1

---
+++ ElibraryManagment/Site1.Master.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ElibraryManagment
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButton6_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminLogin.aspx");
+        }
+
+
+        protected void LinkButton11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
